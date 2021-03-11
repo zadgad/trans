d693046e105375c69bc20c7a0913b38271ad7673
zadgad <cradoblack@gmail.com>
2021-03-11 17:12:44 -0600

    quitando scripts

--- resources/views/layouts/appl.blade.php
+++ resources/views/layouts/appl.blade.php
@@ -108,7 +108,7 @@
             </div>
         </div>
         {{-- <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script> --}}
-        <script>window.jQuery || document.write('<script src="{{asset('proyect') }}./src/js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+        {{-- <script>window.jQuery || document.write('<script src="{{asset('proyect') }}./src/js/vendor/jquery-3.3.1.min.js"><\/script>')</script> --}}
         <script src="{{ asset('proyect') }}./plugins/popper.js/dist/umd/popper.min.js"></script>
         <script src="{{ asset('proyect') }}./plugins/bootstrap/dist/js/bootstrap.min.js"></script>
         <script src="{{ asset('proyect') }}./plugins/perfect-scrollbar/dist/perfect-scrollbar.min.js"></script>
@@ -120,14 +120,14 @@
         <script src="{{ asset('proyect') }}./dist/js/theme.min.js"></script>
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
+        {{-- <script>
             (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
             function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
             e=o.createElement(i);r=o.getElementsByTagName(i)[0];
             e.src='https://www.google-analytics.com/analytics.js';
             r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
             ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-        </script>
+        </script> --}}
         <script>
             //    console.log(user);
                     $(document).ready(function(){
